2927115dc7a0893d8f783ff6817fe8ae1e68d18c
repl1224 <jaywalk1224@gmail.com>
2016-08-26 13:26:55 +0900

    second

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,3 +1,28 @@
+class Circle{
+	public int x, y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [x=" + x + ", y=" + y + "]";
+	}
+	
+}
 
 public class Test {
 	public static void main(String[] args) {
